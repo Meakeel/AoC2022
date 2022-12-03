c43a5eb9589d4d03a028f24d780ccadf36633655
Paul <paul.sparkes@live.com>
2022-12-03 08:38:08 +0000

    Day 3 Looptastic

--- Day3/part1.js
+++ Day3/part1.js
@@ -1,5 +1,6 @@
-let rounds = [];
-let score = 0;
+let backpacks = [];
+let matches = 0;
+let matchArray = [];
 
 let lineReader = require('readline').createInterface({
   input: require('fs').createReadStream('input.txt')
@@ -7,94 +8,63 @@ let lineReader = require('readline').createInterface({
 
 
 lineReader.on('line', function (line) {
-  let split = line.split(" ");
-  rounds.push([split[0], split[1]]);
+  let eachBackpackLength = line.length /2;
+  let first = line.substring(0, eachBackpackLength);
+  let second = line.substring(eachBackpackLength);
+
+  let firstBackpack = [];
+  let secondBackpack = [];
+
+  for (let i = 0; i < first.length; i++) {
+    let code = first.charCodeAt(i);
+    if (code > 96) {
+      firstBackpack.push(code - 96);
+    } else {
+      firstBackpack.push(code - 38);
+    }
+  }
+
+  for (let i = 0; i < second.length; i++) {
+    let code = second.charCodeAt(i);
+    if (code > 96) {
+      secondBackpack.push(code - 96);
+    } else {
+      secondBackpack.push(code - 38);
+    }
+  }
+
+  backpacks.push([firstBackpack, secondBackpack]);
 });
 
 function calcResult() {
-  for (let i = 0; i < rounds.length; i++) {
-    let round = rounds[i];
-    playHand(round[0], round[1]);
+  for (let i = 0; i < backpacks.length; i++) {
+    let backpack = backpacks[i];
+
+    let currentMathes = [];
+    for (let i = 0; i < backpack[0].length; i++) {
+      let firstBackpackItem = backpack[0][i];
+      for (let j = 0; j < backpack[1].length; j++) {
+        let secondBackpackItem = backpack[1][j];
+        if (firstBackpackItem === secondBackpackItem) {
+          if (currentMathes.length === 0) {
+            matches = matches + firstBackpackItem;
+            currentMathes.push(firstBackpackItem);
+            continue;
+          }
+          let anyMatch = currentMathes.reduce((prev, current, index) => current === firstBackpackItem);
+          if (anyMatch === 0) {
+            matches = matches + firstBackpackItem;
+
+            currentMathes.push(firstBackpackItem);
+          }
+        }
+      }
+      
+    }
   }
 
-  console.log(score);
-}
+  console.log(matches);
 
-function playHand(other, you) {
-  switch (you) {
-    // ROCK
-    case "X":
-      score = score + 1;
-
-      switch (other) {
-        // ROCK
-        case "A":
-          score = score + 3;
-          break;
-          // PAPER
-        case "B":
-          score = score + 0;
-
-          break;
-          // SCISSORS
-        case "C":
-          score = score + 6;
-          break;
-
-        default:
-          break;
-      }
-      break;
-      // PAPER
-    case "Y":
-      score = score + 2;
-      switch (other) {
-        // ROCK
-        case "A":
-          score = score + 6;
-          break;
-          // PAPER
-        case "B":
-          score = score + 3;
-
-          break;
-          // SCISSORS
-        case "C":
-          score = score + 0;
-          break;
-
-        default:
-          break;
-      }
-      break;
-      // SCISSORS
-    case "Z":
-      score = score + 3;
-
-      // (0 if you lost, 3 if the round was a draw, and 6 if you won)
-      switch (other) {
-        // ROCK
-        case "A":
-          score = score + 0;
-          break;
-          // PAPER
-        case "B":
-          score = score + 6;
-
-          break;
-          // SCISSORS
-        case "C":
-          score = score + 3;
-          break;
-
-        default:
-          break;
-      }
-      break;
-
-    default:
-      break;
-  }
 }
 
 lineReader.on('close', function () {
--- Day3/part2.js
+++ Day3/part2.js
@@ -1,5 +1,6 @@
-let rounds = [];
-let score = 0;
+let backpacks = [];
+let matches = 0;
+let matchArray = [];
 
 let lineReader = require('readline').createInterface({
   input: require('fs').createReadStream('input.txt')
@@ -7,95 +8,90 @@ let lineReader = require('readline').createInterface({
 
 
 lineReader.on('line', function (line) {
-  let split = line.split(" ");
-  rounds.push([split[0], split[1]]);
+  let lineBackpack = [];
+
+  for (let i = 0; i < line.length; i++) {
+    let code = line.charCodeAt(i);
+    if (code > 96) {
+      lineBackpack.push(code - 96);
+    } else {
+      lineBackpack.push(code - 38);
+    }
+  }
+
+  backpacks.push(lineBackpack);
 });
 
 function calcResult() {
-  for (let i = 0; i < rounds.length; i++) {
-    let round = rounds[i];
-    playHand(round[0], round[1]);
+  let groups = backpacks.length / 3;
+  let count = 0;
+
+  for (let i = 0; i < groups; i++) {
+    let firstBackpack = backpacks[count++];
+    let secondBackpack = backpacks[count++];
+    let thirdBackpack = backpacks[count++];
+
+    let currentMathes = [];
+
+    for (let i = 0; i < firstBackpack.length; i++) {
+      let firstBackpackItem = firstBackpack[i];
+      
+      for (let j = 0; j < secondBackpack.length; j++) {
+        let secondBackpackItem = secondBackpack[j];
+
+        for (let k = 0; k < thirdBackpack.length; k++) {
+          let thirdBackpackItem = thirdBackpack[k];
+
+          if (firstBackpackItem === secondBackpackItem && secondBackpackItem === thirdBackpackItem) {
+            if (currentMathes.length === 0) {
+              matches = matches + firstBackpackItem;
+              currentMathes.push(firstBackpackItem);
+              continue;
+            }
+
+            let anyMatch = currentMathes.reduce((prev, current, index) => current === firstBackpackItem);
+            if (anyMatch === 0) {
+              matches = matches + firstBackpackItem;
+  
+              currentMathes.push(firstBackpackItem);
+            }
+          }
+          
+        }
+        
+      }
+    }
   }
 
-  console.log(score);
-}
 
-// X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win. Good luck!
-function playHand(other, you) {
-  switch (you) {
-    // LOSE
-    case "X":
-      score = score + 0;
-      score = score + losePlayedHand(other);
-      break;
-      // DRAW
-    case "Y":
-      score = score + 3;
-      score = score + drawPlayedHand(other);
-      break;
-      // WIN
-    case "Z":
-      score = score + 6;
-      score = score + winPlayedHand(other);
-      break;
+  // for (let i = 0; i < backpacks.length; i++) {
+  //   let backpack = backpacks[i];
 
-    default:
-      break;
-  }
-}
+  //   let currentMathes = [];
+  //   for (let i = 0; i < backpack[0].length; i++) {
+  //     let firstBackpackItem = backpack[0][i];
+  //     for (let j = 0; j < backpack[1].length; j++) {
+  //       let secondBackpackItem = backpack[1][j];
+  //       if (firstBackpackItem === secondBackpackItem) {
+  //         if (currentMathes.length === 0) {
+  //           matches = matches + firstBackpackItem;
+  //           currentMathes.push(firstBackpackItem);
+  //           continue;
+  //         }
+  //         let anyMatch = currentMathes.reduce((prev, current, index) => current === firstBackpackItem);
+  //         if (anyMatch === 0) {
+  //           matches = matches + firstBackpackItem;
 
-function losePlayedHand(other) {
-  // 1 for Rock, 2 for Paper, and 3 for Scissors
-  switch (other) {
-    // ROCK
-    case "A":
-      return 3;
-      // PAPER
-    case "B":
-      return 1;
-      // SCISSORS
-    case "C":
-      return 2;
+  //           currentMathes.push(firstBackpackItem);
+  //         }
+  //       }
+  //     }
 
-    default:
-      break;
-  }
-}
+  //   }
+  // }
 
-function drawPlayedHand(other) {
-  // 1 for Rock, 2 for Paper, and 3 for Scissors
-  switch (other) {
-    // ROCK
-    case "A":
-      return 1;
-      // PAPER
-    case "B":
-      return 2;
-      // SCISSORS
-    case "C":
-      return 3;
+  console.log(matches);
 
-    default:
-      break;
-  }
-}
-
-function winPlayedHand(other) {
-  // 1 for Rock, 2 for Paper, and 3 for Scissors
-  switch (other) {
-    // ROCK
-    case "A":
-      return 2;
-      // PAPER
-    case "B":
-      return 3;
-      // SCISSORS
-    case "C":
-      return 1;
-
-    default:
-      break;
-  }
 }
 
 lineReader.on('close', function () {
